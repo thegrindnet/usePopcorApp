7a11f35c62035252e2e409144911b3b222831fe5
David Bonilla <thegrindnet@gmail.com>
2026-06-29 22:55:41 -0600

    Change overflow to hidden in box and list classes

--- usepopcorn/src/App.js
+++ usepopcorn/src/App.js
@@ -69,19 +69,26 @@ function NavBar() {
           <span role="img">🍿</span>
           <h1>usePopcorn</h1>
         </div>
-        <input
-          className="search"
-          type="text"
-          placeholder="Search movies..."
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-        />
+        <Search />
         <p className="num-results">
           Found <strong>X</strong> results
         </p>
       </nav>
       ;
     </>
+  );
+}
+
+function Search() {
+  const [query, setQuery] = useState("");
+  return (
+    <input
+      className="search"
+      type="text"
+      placeholder="Search movies..."
+      value={query}
+      onChange={(e) => setQuery(e.target.value)}
+    />
   );
 }
 
